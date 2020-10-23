9bdc991c14940f6f5abaddf23657e36b3817b135
Martyna Golas <D00193013@student.dkit.ie>
2020-10-23 22:50:41 +0100

    Added the text file for I/O. Created a method that reads in the file and adds all of the elements to the array. Changed the name of the temp array. Closed the i/o stream.

--- src/CA1/JcResults.java
+++ src/CA1/JcResults.java
@@ -2,6 +2,7 @@ package CA1;
 
 import java.util.Scanner;
 import java.io.File;
+import java.io.FileNotFoundException;
 
 public class JcResults
 {
@@ -12,6 +13,30 @@ public class JcResults
 
     public static void readInFile()
     {
-
+        try
+        {
+            Scanner scan = new Scanner(new File("JC_Results.txt"));
+            while(scan.hasNextLine())
+            {
+                scan.useDelimiter("[,|\r\n]");
+                int[] temp = new int[34];
+                for(int i = 0; i < temp.length; i++)
+                {
+                    if(scan.hasNextInt())
+                    {
+                        temp[i] = scan.nextInt();
+                    }
+                    else if(scan.hasNext())
+                    {
+                        scan.nextLine();
+                    }
+                }
+            }
+        }
+        catch(FileNotFoundException e)
+        {
+            System.out.println("Cannot find the file.");
+        }
+        scan.close();
     }
 }
